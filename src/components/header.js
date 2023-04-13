6ad737e99de1ea6d6bacb98a8b067641b11f8b87
Manav Misra <manavm@visionwebsoft.com>
2023-04-13 16:21:31 -0500

    Use  a function component to render a Header

---
+++ src/components/header.js
@@ -0,0 +1,3 @@
+export default function Header({text}) {
+  return `<h1 class="text-red-500 text-center text-3xl my-4">${text}</h1>`
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,12 @@
-import data from './data';
+import Header from './components/header';
 
 // DOM - Document Object Model
 // DOM is a JS object that represents the nodes (parents/children) of the HTML document
 const root = document.getElementById('root');
 
-root.innerHTML = '<h1 class="text-red-500 text-center text-3xl my-4">Hello World</h1>'
+
+
+root.innerHTML = Header(
+
+  // Simulate a 'props' object from React
+  {text: "Hello Function Component"});
