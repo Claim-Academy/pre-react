32155bd1cb203353b10f2e02a5dca7660b87eef2
Manav Misra <manavm@visionwebsoft.com>
2023-04-13 15:58:53 -0500

    Use JS to add an ht to the root

---
+++ src/index.js
@@ -0,0 +1,5 @@
+// DOM - Document Object Model
+// DOM is a JS object that represents the nodes (parents/children) of the HTML document
+const root = document.getElementById('root');
+
+root.innerHTML = '<h1 class="text-red-500 text-center text-3xl my-4">Hello World</h1>'
